8a0037079fe803dc934b1b134ee70a1a31b05caa
Rolf W. Rasmussen <rolfwr@gmail.com>
2019-08-15 23:55:25 +0200

    Clean-up and dist building.

--- index.ts
+++ index.ts
@@ -4,8 +4,20 @@
 import 'reveal.js/css/reveal.css';
 import 'reveal.js/css/theme/moon.css';
 import 'reveal.js/lib/css/monokai.css'; 
+import './styling.css';
 
 const Reveal = require('reveal.js/js/reveal.js');
+
+// Reveal.js plugin loading expects to find the Reveal instance here.
+declare global {
+    interface Window { Reveal: any; }
+}
+
+window.Reveal = Reveal;
+
 Reveal.initialize({
-    history: true
+    history: true,
+    dependencies: [
+        { src: 'highlight.js', async: true }
+    ]
 });
